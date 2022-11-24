d13bbf525f6529b6ae54f72b14bf54c05c237e9d
Matheus N. Borges <matheusnborgesdev@gmail.com>
2022-11-24 14:56:17 -0300

    calculadora

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+
+root.render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+
+  </div>
+
+
+ );
+
+
